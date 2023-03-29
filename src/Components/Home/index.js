6d9b11c9f78559408bcc0203cfc891ae4f150c07
Siddharth Jain <siddharthjainjio@gmail.com>
2023-03-30 00:18:37 +0530

    Fix

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -21,6 +21,7 @@ function Home() {
   ])
 
   const [isTyping, setIsTyping] = useState(false);
+  const myAppRef = document.querySelector('.scrollable-div')
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -38,7 +39,7 @@ function Home() {
       model: "gpt-3.5-turbo",
       messages: [...newMessages],
     });
-
+    
     messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     setMessages([...newMessages, completion.data.choices[0].message]);
     setIsTyping(false);
@@ -53,18 +54,15 @@ function Home() {
       }
     ]);
   }
-  const myAppRef = document.querySelector('.scrollable-div')
 
   return (
     <div>
-
       {currentUser ?
         (
           <section className='container mx-auto p-0 pt-2 pb-2 fixed inset-0 mt-4'>
             <div className="mockup-window bg-base-300 w-full h-full flex flex-col">
-              <div className="p-0 pb-8 flex-grow overflow-auto">
-                {
-                  messages.length && messages.map((msg, i) => {
+              <div className="p-0 pb-8 flex-grow overflow-auto" style={{marginBottom:"50px"}}>
+                {messages.length && messages.map((msg, i) => {
                     return (
                       <div className={`chat ${msg.role === 'assistant' ? 'chat-start' : 'chat-end'}`} key={i}>
                         <div className="chat-image avatar">
@@ -73,14 +71,18 @@ function Home() {
                           </div>
                         </div>
                         <div className="chat-bubble" ref={messagesEndRef}>
-                          {msg.role === 'assistant' ? <TypeWriterEffect
-                            startDelay={40}
-                            cursorColor="transparent"
-                            text={msg.content}
-                            typeSpeed={50}
-                            scrollArea={myAppRef}
-                          /> :
-                             msg.content 
+                          {msg.role === 'assistant' ?
+                            <pre className='word-wrap'>
+                              <TypeWriterEffect
+                                startDelay={40}
+                                cursorColor="transparent"
+                                text={msg.content}
+                                typeSpeed={50}
+                                scrollArea={myAppRef}
+                              />
+                            </pre>
+                            :
+                            <pre>{msg.content}</pre>
                           }
                         </div>
                       </div>
@@ -88,23 +90,24 @@ function Home() {
                   })
                 }
               </div>
-
-              <form className='form-control p-0 items-center bg-transparent border-0 text-light' onSubmit={(e) => handleSubmit(e)}>
-                <div className="input-group max-w-full w-[800px] relative">
-                  {isTyping && <small className='absolute -top-6 left-0.5 animate-pulse'>Wednesday is typing...</small>}
-                  <input type="text" placeholder='Type a question for SidGPT, ask anything!' className='input input-bordered flex-grow' required />
-                  <button className='btn btn-square mr-1' type='submit'>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 16 16">
-                      <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                    </svg>
-                  </button>
-                  <button className='btn btn-danger' type="button" onClick={(e) => handleRestart(e)}>
-                    <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path>
-                    </svg>&nbsp;
-                    Restart chat
-                  </button>
-                </div>
-              </form>
+              <div className="p-0 fixed-bottom flex-grow overflow-auto text-center">
+                <form className='form-control p-0 items-center bg-transparent border-0 text-light' onSubmit={(e) => handleSubmit(e)}>
+                  <div className="input-group max-w-full w-[800px] relative">
+                    {isTyping && <small className='absolute -top-6 left-0.5 animate-pulse'>Wednesday is typing...</small>}
+                    <input type="text" placeholder='Type a question for SidGPT, ask anything!' className='input input-bordered flex-grow' required />
+                    <button className='btn btn-square mr-1' type='submit'>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                      </svg>
+                    </button>
+                    <button className='btn btn-danger' type="button" onClick={(e) => handleRestart(e)}>
+                      <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><polyline points="1 4 1 10 7 10"></polyline><polyline points="23 20 23 14 17 14"></polyline><path d="M20.49 9A9 9 0 0 0 5.64 5.64L1 10m22 4l-4.64 4.36A9 9 0 0 1 3.51 15"></path>
+                      </svg>&nbsp;
+                      Restart chat
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </section>
         )

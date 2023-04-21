76d7d44dc9f2c8c8dabe46511d1f63e61b767433
Siddharth Jain <siddharthjainjio@gmail.com>
2023-04-21 16:59:14 +0530

    Update

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -77,7 +77,7 @@ function Home() {
                   <form className='form-control p-0 items-center bg-transparent border-0 text-light' onSubmit={(e) => handleSubmit(e)}>
                     <div className="input-group max-w-full w-[1520px] relative">
                       {isTyping && <small className='absolute -top-6 left-1.5 animate-pulse'>Siddharth is typing...</small>}
-                      <textarea type="text" placeholder='Type a question for SidGPT, ask anything!' autoresize className='input input-bordered flex-grow pt-2 mr-2 rounded-5' required />
+                      <textarea type="text" placeholder='Type a question, ask anything!' autoresize className='input input-bordered flex-grow pt-2 mr-2 rounded-5' required />
                       <button className='btn btn-square mr-1 rounded-5' type='submit'>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 16 16">
                           <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
